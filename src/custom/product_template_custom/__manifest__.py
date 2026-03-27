0e6936d357ac01220074d93ddf30b3e82e55bd46
agamboa <agamboa@prolesys.com>
2026-03-27 14:13:57 -0400

    Feat: Implementacion de la estructura de carpetas correspondiente a odoo. - creacion de manifiesto cumpliendo con os requisitos del modulo - creacion de la modularizacion de python correspondiente - creacion de archivos vacios para posterior implementacion, tanto de testing como el modelo

---
+++ src/custom/product_template_custom/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Inventario - Alertas',
+    'version': '18.0.0.0.0',
+    'category': 'Inventory',
+    'summary': 'Alertas de stock crítico para productos',
+    'author': 'Tu Nombre',
+    'depends': ['stock', 'mail'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
